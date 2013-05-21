75e9f0d0f07a8137d5874491d176a42914ac793a
Bobby <bellfalasch@gmail.com>
2013-05-21 17:03:31 +0200

    Changed table name

--- _admin/DATABASE.sql
+++ _admin/DATABASE.sql
@@ -10,12 +10,6 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
 --
 -- Database: `test`
 --
@@ -26,7 +20,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `nxtcms_users`
 --
 
-CREATE TABLE IF NOT EXISTS `nxtcms_users` (
+CREATE TABLE IF NOT EXISTS `cms_users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `mail` varchar(255) NOT NULL,
@@ -38,12 +32,8 @@ CREATE TABLE IF NOT EXISTS `nxtcms_users` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
--- Dumping data for table `nxtcms_users`
+-- Dumping data for table `cms_users`
 --
 
-INSERT INTO `nxtcms_users` (`id`, `name`, `mail`, `username`, `password`, `lastlogin`, `level`) VALUES
-(1, NULL, 'test@test.no', 'Nils', 'pills', '2012-02-29 12:45:30', 2);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `cms_users` (`id`, `name`, `mail`, `username`, `password`, `lastlogin`, `level`) VALUES
+(1, NULL, 'admin@test.no', 'admin', 'password', '2012-02-29 13:37:00', 2);
